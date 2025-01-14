ff1149fda44d12a468f79028e0f01eb2f3f462cd
Rahat65 <m.rahatdrmc65@gmail.com>
2025-01-14 16:07:07 +0600

    Initial commit

---
+++ test/practice_code_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:practice_code_flutter/practice_code_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
